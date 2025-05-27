04a9630fd818b07c37143c4ae6d61884a76606b2
Tonkata28 <tonisimeonov33@gmail.com>
2025-05-27 23:38:42 +0300

    Added play_again() and range_change_function()

--- guess_the_num_2.py
+++ guess_the_num_2.py
@@ -8,25 +8,28 @@ def validating_start_end():
         beginning_1 = (input("\n Choose the start of the range for your game(integer): "))
         ending_1 = (input("\n Choose the end of the range for your game(integer): "))
 
-        if not beginning_1.isdecimal() or not ending_1.isdecimal():
+        if beginning_1.strip("-").isdigit() and ending_1.strip("-").isdigit():
 
-            print("\n Starting and ending must be integers!")
+            beginning_1, ending_1 = int(beginning_1), int(ending_1)
+
+        else:
+            print("\n Start and end of range must be integers! ")
             continue
 
-        beginning_1, ending_1 = int(beginning_1), int(ending_1)
-
         if beginning_1 < ending_1:
+
             return beginning_1, ending_1
+
         else:
             print("\n Invalid input. Start of range can't be bigger than or equal to the end.")
 
 
-def yes_answer(question: str) -> bool:
-    return "yes" == question.lower().strip()
+def yes_answer(answer: str) -> bool:
+    return "yes" == answer.lower().strip()
 
 
-def no_answer(question: str) -> bool:
-    return "no" == question.lower().strip()
+def no_answer(answer: str) -> bool:
+    return "no" == answer.lower().strip()
 
 
 def end_guess(guess: str) -> bool:
@@ -42,7 +45,8 @@ def navigation_user(guess: int, special_num: int, tries: int, counter: int) -> N
         print(f"\n The number is smaller than your guess. {tries - counter} tries remaining.")
 
 
-def validating_guess(guess: str, ending_1: int, starting: int) -> bool or None:
+def validating_guess(guess: str, ending_1: int, starting: int) -> \
+        bool or None:
 
     end_flag = end_guess(guess)
 
@@ -50,7 +54,7 @@ def validating_guess(guess: str, ending_1: int, starting: int) -> bool or None:
         print("\n End of the game! Forced manually!")
         return None
 
-    elif not guess.isdigit():
+    elif not guess.strip("-").isdigit():
         print("\n Invalid input. Your guess must be an integer!")
         return False
 
@@ -60,6 +64,44 @@ def validating_guess(guess: str, ending_1: int, starting: int) -> bool or None:
 
     else:
         return True
+
+
+def range_change_function(beginning: int, ending: int) -> bool or tuple[int, int]:
+
+    range_change = input("\n Would you like to change the range?"
+                         "\n Answer with 'yes' or 'no' :")
+
+    if no_answer(answer=range_change):
+
+        print(f"\n Ok, continuing with the range({beginning} - {ending}). ")
+
+        return beginning, ending
+
+    elif yes_answer(answer=range_change):
+
+        beginning, ending = validating_start_end()  # For better readability
+        return beginning, ending
+
+    else:
+        print("\n Invalid input for changing the range. End of game! ")
+        return False
+
+
+def play_again() -> bool:
+
+    play_again_answer = input(f"\n Would you like to play again?\n Answer with 'yes' or 'no': ")
+
+    if no_answer(answer=play_again_answer):
+
+        print("\n End of the game. See you soon!")
+        return False
+
+    elif not yes_answer(answer=play_again_answer):
+
+        print("\n Invalid input! Ending game. ")
+        return False
+
+    return True
 
 
 def play_game():
@@ -77,33 +119,17 @@ def play_game():
         if not guessing_the_num(start=beginning, end= ending, tries=attempts):
             return
 
-        play_again = input(f"\n Would you like to play again?\n Answer with 'yes' or 'no': ")
+        play_again_return = play_again()
 
-        if no_answer(question=play_again):
-
-            print("\n End of the game. See you soon!")
+        if not play_again_return:
             return
 
-        elif not yes_answer(question=play_again):
+        range_change_return = range_change_function(beginning=beginning, ending=ending)
 
-            print("\n Invalid input! Ending game. ")
+        if not range_change_return:
             return
 
-        range_change = input("\n Would you like to change the range?"
-                             "\n Answer with 'yes' or 'no' :")
-
-        if no_answer(question=range_change):
-
-            print(f"\n Ok, continuing with the range({beginning} - {ending}). ")
-            continue
-
-        elif yes_answer(question=range_change):
-
-            beginning, ending = validating_start_end()
-
-        else:
-            print("\n Invalid input for changing the range. End of game! ")
-            return
+        beginning, ending = range_change_return
 
 
 def guessing_the_num(start: int, end: int, tries: int) -> bool:
@@ -139,12 +165,14 @@ def guessing_the_num(start: int, end: int, tries: int) -> bool:
 
 
 def choosing_difficulty() -> int or None:
+
     mode = (input("\n Which mode would you like to play?\n"
             f" There are currently {modes_available} modes.\n"
-                  " 'Easy - 1'\n"
-                  " 'Normal - 2'\n"
-                  " 'Hard - 3'\n"
-                  " 'Sniper - 4'\n"
+                  
+                  " 'Easy - 1 (15 total tries)'\n"
+                  " 'Normal - 2 (10 total tries)'\n"
+                  " 'Hard - 3 (5 total tries)'\n"
+                  " 'Sniper - 4 (1 try)'\n"
                   " Enter your corresponding number here: "))
 
     if not mode.isdecimal():
